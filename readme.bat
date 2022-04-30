53fb4088a341f006b9d58476018fadc14c0c7d30
SoyNashi <79770903+SoyNashi@users.noreply.github.com>
2022-04-30 20:02:12 +0200

    Update readme.bat

---
+++ readme.bat
@@ -0,0 +1,44 @@
+@echo off
+color 03
+set file=README.md
+:start
+set /p direction= Dir- 
+set /p name= Name- 
+set /p desc= Description- 
+Set /p link= https://soynashi.tk/
+echo ##%name% > %file%
+echo %desc% >> %file%
+echo --- >> %file%
+echo Made by Nashi >> %file%
+echo https://soynashi.tk/%link% >> %file%
+
+echo # Nashi >> %file%
+echo ##### - ?? Hola, me llamo @SoyNashi >> %file%
+echo ##### - ?? Normalmene codifico webs pero aqui seguramente publique juegos gratis >> %file%
+echo ##### - ?? Visita mi web https://soynashi.tk >> %file%
+echo ## Donaciones  >> %file%
+echo > Programo por diversión, pero tengo que ser capaz de mantenerme vivo y aunque no sea suficiente como para mi supervivencia, algo es algo y me puede ayudar a hacer más proyectos. Mis proyectos web tiene un tipo de monetización sin anuncios, que no rompen la vista de la web, pero me generan algo de ingresos. De los NFT no vendo nada, pero están ahí por si acaso se llegan a popularizar y alguien me lo compra. De todo lo otro no genero nada, pero tengo un PayPal por si alguna buena alma decide donar. >> %file%
+echo https://paypal.me/soynashi >> %file%
+echo ## NFT >> %file%
+echo > Si alguna cosa es la que me ha apoyado a hacer mis proyectos es la fe, y tengo mucha fe en que algo de lo que haga se consiga. Los NFT son algo complicados de entender y no me voy a poner a explicar su funcionamiento, breve resumen, los NFT son obras de arte (música, dibujos, fotos) encriptadas en una criptomoneda. He creado unos cuantos y los estoy vendiendo por OpenSea a ver si alguien ofrece algo. >> %file%
+echo https://soynashi.tk/info/nft/index.html >> %file%
+echo ## Musica >> %file%
+echo > También tengo como hobby hacer música, no soy muy bueno pero le voy pillando práctica. Te invito a mirar mi canal de youtube o mi cuenta de opensea para que puedas escuchar todas mis canciones y remixes. >> %file%
+echo https://soynashi.tk/info/music/index.html >> %file%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
